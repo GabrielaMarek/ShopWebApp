90244a446e8834ba6f4b918893478816274bdc24
Gabriela Marek <w67497@student.wsiz.edu.pl>
2024-02-20 14:28:18 +0100

    Update

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -10,12 +10,18 @@ namespace WebApp.Controllers
             var categories = CategoriesRepository.GetCategories();
             return View(categories);
         }
-
+        [HttpGet]
         public IActionResult Edit(int? id)
         {
             var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
 
             return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
